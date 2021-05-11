9ca365ac72e09d634654e9b11b585f2a248263e7
Brian Downs <brian.downs@gmail.com>
2021-05-11 16:59:33 -0700

    Add the ability to list etcd snapshots (#3303)
    * add ability to list local and s3 etcd snapshots
    
    Signed-off-by: Brian Downs <brian.downs@gmail.com>

--- cmd/k3s/main.go
+++ cmd/k3s/main.go
@@ -37,7 +37,7 @@ func main() {
 		cmds.NewCtrCommand(externalCLIAction("ctr")),
 		cmds.NewCheckConfigCommand(externalCLIAction("check-config")),
 		cmds.NewEtcdSnapshotCommand(wrap(version.Program+"-"+cmds.EtcdSnapshotCommand, os.Args)),
-		cmds.NewEtcdSnapshotCommand(etcdsnapshotCommand, cmds.NewEtcdSnapshotSubcommands(etcdsnapshotCommand)),
+		cmds.NewEtcdSnapshotCommand(etcdsnapshotCommand, cmds.NewEtcdSnapshotSubcommands(etcdsnapshotCommand, etcdsnapshotCommand)),
 	}
 
 	if err := app.Run(os.Args); err != nil {
